85f0bec85893418060d127870d55f1b7a293e2c8
Oluwabammydu <39812151+Oluwabammydu@users.noreply.github.com>
2018-10-09 18:09:00 +0100

    Delete databaseSchema.py

--- databaseSchema.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector as mariadb
-
-mariadb_connection = mariadb.connect(host='localhost', user='root', password='bammybammy')
-
-cursor = mariadb_connection.cursor()
-
-cursor.execute("CREATE DATABASE samm")
